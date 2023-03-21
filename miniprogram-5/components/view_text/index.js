7c3ed97fe2a66d8b796fb89b24d0b29fe085e801
Zhuge <785916801@qq.com>
2023-03-21 19:40:30 +0800

    add mvlist

---
+++ miniprogram-5/components/view_text/index.js
@@ -0,0 +1,26 @@
+// components/view_text/index.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        hello:{
+            type:String,
+            value:"hello"
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
